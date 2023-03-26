2ddfb5e98ee76e31e0fb575cd76a418310f7e812
K.D. Hubbard <hubbard.kd@gmail.com>
2023-03-26 10:54:40 -0600

    add jb instantiation

--- jungle_beat_wrapper.rb
+++ jungle_beat_wrapper.rb
@@ -2,3 +2,7 @@ require "./lib/linked_list"
 require "./lib/node"
 require "lib/jungle_beat"
 
+jb = JungleBeat.new
+
+
+
---
+++ lib/jungle_beat.rb
@@ -0,0 +1,10 @@
+class JungleBeat 
+  attr_reader :list
+  def intialize()
+    @list = LinkedList.new
+  end
+  # def new_song(name)
+  #   name = LinkedList.new
+  #   @list = name
+  # end
+end
